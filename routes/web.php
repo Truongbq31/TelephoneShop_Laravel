98ba0ca54fffeeb783849e92d51df57d975f31d3
truongbq31 <ytbsadboy@gmail.com>
2023-07-17 18:20:14 +0700

    admin templates

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,5 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/telephone/admin', [\App\Http\Controllers\AdminController::class, 'index']);
+Route::get('/telephone/admin/products', [\App\Http\Controllers\AdminController::class,'products']);
