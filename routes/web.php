bab069d4cd970a252eabce6ae8b2511c03bb03b0
manhtuong <lamt5805@gmail.com>
2023-07-19 23:22:02 +0700

    Edit Products

--- routes/web.php
+++ routes/web.php
@@ -24,7 +24,7 @@ Route::get('/telephone/admin/products', [\App\Http\Controllers\ProductsControlle
 Route::match(['GET', "POST"],'/telephone/admin/products/add',[\App\Http\Controllers\ProductsController::class,'addProducts'])->name('route_admin_addProducts');
 Route::match(['GET', "POST"],'/telephone/admin/products/edit/{id}',[\App\Http\Controllers\ProductsController::class,'editProducts'])->name('route_admin_editProducts');
 //Categories
-Route::get('/telephone/admin/categories', [\App\Http\Controllers\CategoriesController::class,'categories'])->name('route_admin_categories');
-Route::match(['GET', "POST"],'/telephone/admin/categories/add',[\App\Http\Controllers\CategoriesController::class,'addCategories'])->name('route_admin_addCategories');
-Route::match(['GET', "POST"],'/telephone/admin/categories/edit/{id}',[\App\Http\Controllers\CategoriesController::class,'editCategories'])->name('route_admin_editCategories');
+Route::get('/telephone/admin/categories', [\App\Http\Controllers\Admin\CategoriesController::class,'categories'])->name('route_admin_categories');
+Route::match(['GET', "POST"],'/telephone/admin/categories/add',[\App\Http\Controllers\Admin\CategoriesController::class,'addCategories'])->name('route_admin_addCategories');
+Route::match(['GET', "POST"],'/telephone/admin/categories/edit/{id}',[\App\Http\Controllers\Admin\CategoriesController::class,'editCategories'])->name('route_admin_editCategories');
 //End admin
